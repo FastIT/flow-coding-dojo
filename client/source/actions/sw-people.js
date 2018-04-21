69c8c97e5b75ed7b75248a5d1a08d2c1c245a614
Hugo CAPOCCI <hcapocci@degetel.com>
2018-04-21 20:23:22 +0200

    Type action

--- client/source/actions/sw-people.js
+++ client/source/actions/sw-people.js
@@ -1,5 +1,13 @@
+// @flow
 import cst from '../constants/sw-people';
+import type { People } from '../common/people';
 
-const setPeopleList = (peopleList) => ({ type: cst.SET_PEOPLE_LIST, payload: peopleList });
+export type SetPeopleListAction = {
+  type: cst.SET_PEOPLE_LIST,
+  payload: Array<People>,
+};
+
+const setPeopleList = (peopleList: Array<People>): SetPeopleListAction =>
+  ({ type: cst.SET_PEOPLE_LIST, payload: peopleList });
 
 export default setPeopleList;
